71f4aaef4044b84db3bce5523e318b19cd15756c
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2016-12-02 21:39:31 -0500

    Start user mock API

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -20,6 +20,14 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  res.json([
+    {"id": 1, "firstName": "Bob", "lastName": "Marley", "email": "bob@gmail.com"},
+    {"id": 2, "firstName": "Tammy", "lastName": "Wynette", "email": "tammy@yahoo.com"},
+    {"id": 3, "firstName": "Tina", "lastName": "Turner", "email": "tina@hotmail.com"}
+  ]);
+});
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
--- src/index.test.js
+++ src/index.test.js
@@ -9,11 +9,11 @@ describe('Our first test', function() {
 });
 
 describe('index.html', function() {
-  it('should say hello', function(done) {
+  it('should have a Users heading', function(done) {
     const index = fs.readFileSync('./src/index.html', "utf-8");
     jsdom.env(index, function(err, window) {
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal('Sample Hello World App');
+      expect(h1.innerHTML).to.equal('Users');
       done();
       window.close();
     })
