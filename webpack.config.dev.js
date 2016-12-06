046179570ac914513e96b3cba5205ebfd553f832
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2016-12-05 21:08:47 -0500

    Add production build features

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
   debug: true,
@@ -13,17 +14,23 @@ export default {
     publicPath: '/',
     filename: 'bundle.js'
   },
-  plugins: [],
+  plugins: [
+    // Create HTML file that includes references to bundled JS
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    })
+  ],
   module: {
     loaders: [
       {
         test: /\.js$/,
-      	exclude: /node_modules/,
-      	loaders: ['babel']
+        exclude: /node_modules/,
+        loaders: ['babel']
       },
       {
         test: /\.css$/,
-      	loaders: ['style','css']
+        loaders: ['style','css']
       }
     ]
   }
